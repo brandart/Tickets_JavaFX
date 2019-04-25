9e06b93b6dd3135972c2e578dfe03dad2ab11834
brandart <brandstetterarthur@gmail.com>
2019-04-25 08:34:49 +0200

    added Server

---
+++ U_2019_04_04_Server/U_2019_04_04_Server/Service.svc.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Services;
+using System.Data.Services.Providers;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace U_2019_04_04_Server
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service.svc or Service.svc.cs at the Solution Explorer and start debugging.
+    public class Service : EntityFrameworkDataService<TicketDbEntities>
+    {
+
+        public static void InitializeService(DataServiceConfiguration config)
+        {
+            config.SetEntitySetAccessRule("*", EntitySetRights.All);
+            config.DataServiceBehavior.MaxProtocolVersion = System.Data.Services.Common.DataServiceProtocolVersion.V3;
+        }
+    }
+}
